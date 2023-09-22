6d2918ac7febbb821d48ab6211c9babd35852952
ahmedforhad <aq182974@gmail.com>
2023-09-22 02:17:08 -0400

    updated

---
+++ src/Forhad_homework/Arraytask1.java
@@ -0,0 +1,19 @@
+package Forhad_homework;
+
+public class Arraytask1 {
+
+		public static void main(String[] args) {
+			int [] months=new int[10];
+			months[1]=4;
+			months[7]=22;
+			months[9]=44;
+			System.out.println("The length of the array "+months.length);
+			System.out.println("all the value below:");
+			for(int newMonths:months) {
+				System.out.println(newMonths);
+			}
+		}
+}
+			
+		
+	
